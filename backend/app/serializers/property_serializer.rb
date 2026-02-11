5c8596ecf0e6c19bdc4865cf13a010b3ee5461f7
Fernando Castellanos <fernando.castellanos@gmail.com>
2026-02-11 09:54:32 -0800

    Creating class PropertyDescriptionUpdaterService and rake task to call OpenaiPropertyDescriptionService to update Property descriptions

--- backend/app/serializers/property_serializer.rb
+++ backend/app/serializers/property_serializer.rb
@@ -1,5 +1,5 @@
 class PropertySerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :city, :state, :zip, :latitude, :longitude
+  attributes :id, :name, :address, :city, :state, :zip, :latitude, :longitude, :description
 
   has_many :posts
 end
